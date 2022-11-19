33c5b3e8a800e6d55c6f6013fac00aba92dd7241
Liam Murphy <liam@phmurphy.com>
2022-11-19 11:45:33 -0800

    add some vmware stuff

--- nixos/vm/default.nix
+++ nixos/vm/default.nix
@@ -30,18 +30,18 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.utf8";
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
+    enable = true;
+    videoDrivers = [ "vmware" ];
     layout = "us";
     xkbVariant = "";
   };
+  hardware.opengl.enable = true;
 
   # Enable sound with pipewire.
   sound.enable = true;
